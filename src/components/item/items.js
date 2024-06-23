1a2980870f16d9391887483d4b3e632f14312aaa
anis safia <anissafia110@gmail.com>
2024-06-23 22:04:16 +0100

    add a new column for quantity with two buttons +/-

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const { items, del, updateItems } = props;
+  let length = items.length;
+  const increaseQuantity = (id) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    updateItems(updatedItems);
+  };
 
-export default Items
+  const decreaseQuantity = (id) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === id && item.quantity > 0) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    updateItems(updatedItems);
+  };
+
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <div className="quantity-controls">
+            <button onClick={() => decreaseQuantity(item.id)}>-</button>
+            <span>{item.quantity}</span>
+            <button onClick={() => increaseQuantity(item.id)}>+</button>
+          </div>
+
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
